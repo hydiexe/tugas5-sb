2abf8c383ee0f74ff670adb655b97a51b0949b5c
hydiexe <nexuszeoviennazabrizkie@gmail.com>
2023-10-13 20:29:01 +0700

    first commit

---
+++ components/AppNav.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="p-3 flex items-center gap-3 bg-blue-600 text-white">
+		<div class="flex-1">
+			<nuxt-link to="/" class="text-2xl font-bold">
+				Artikle Tugas 5 - Sekolah Beta
+			</nuxt-link>
+		</div>
+		<div class="flex-0">
+			hydie's article
+		</div>
+	</nav>
+</template>
